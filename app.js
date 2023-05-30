012622f0c655f8f2412e9ea44dac8c92b71f35c3
Chi <cheez2012@gmail.com>
2023-05-29 18:10:51 -0700

    webhook to zapier using axios fixed button allegedly - submit in modal goes to zapier /delete command might work

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const axios = require('axios');
 const { App, LogLevel } = require('@slack/bolt');
 
 const slackApp = new App({
@@ -118,103 +118,66 @@ slackApp.message('hi', async ({ message, say, client }) => {
   say(buttonMessage);
 });
 
-slackApp.action('Create SoW', async ({ ack, body, client, respond }) => {
+slackApp.action('Create SoW', async ({ ack, body, client }) => {
   await ack();
 
-  // Disable the button and replace it with a message
-  await respond({
-    replace_original: true,
-    text: "Processing...",
-    blocks: [
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: "Processing..."
-        }
-      }
-    ]
-  });
-
-  // Ask the user for the company name or POC using blocks
-  await respond({
-    text: "Who are we doing this project for? Respond with a company name or the name of the point of contact (POC).",
-    blocks: [
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: "Who are we doing this project for? Respond with a company name or the name of the point of contact (POC).",
-        }
-      }
-    ]
-  });
-
-  // Open the modal
-  try {
-    await client.views.open({
-      trigger_id: body.trigger_id,
-      view: {
-        type: 'modal',
-        callback_id: 'sow_modal',
-        title: {
-          type: 'plain_text',
-          text: 'Create SoW'
+  await client.views.open({
+    trigger_id: body.trigger_id,
+    view: {
+      type: 'modal',
+      callback_id: 'sow_modal',
+      private_metadata: JSON.stringify({ userId: body.user.id, channelId: body.channel.id }),
+      title: {
+        type: 'plain_text',
+        text: 'Create a Statement of Work',
+      },
+      blocks: [
+        {
+          block_id: 'company_name_block',
+          type: 'input',
+          label: {
+            type: 'plain_text',
+            text: 'Company name',
+          },
+          element: {
+            action_id: 'company_name_input',
+            type: 'plain_text_input',
+          },
         },
-        blocks: [
-          {
-            type: 'input',
-            block_id: 'company_name_block',
-            label: {
-              type: 'plain_text',
-              text: 'Company Name or POC',
-            },
-            element: {
-              type: 'plain_text_input',
-              action_id: 'company_name_input'
-            }
-          }
-        ],
-        submit: {
-          type: 'plain_text',
-          text: 'Submit'
-        }
-      }
-    });
-  } catch (error) {
-    console.error(`Error in 'Create SoW' action: ${error.message} Stack: ${error.stack}`);
-  }
+      ],
+      submit: {
+        type: 'plain_text',
+        text: 'Submit',
+      },
+    },
+  });
+});
+
+slackApp.view('sow_modal', async ({ ack, body, view, client }) => {
+  await ack();
+
+  const user_input = view.state.values.company_name_block.company_name_input.value;
+  const { userId, channelId } = JSON.parse(view.private_metadata);
+
+  await client.chat.postMessage({
+    channel: channelId,
+    text: `The submitted value is: ${user_input}`,
+  });
+
+  // Save user_input somewhere
+  // Send it to a Zapier webhook
+  axios.post('https://hooks.zapier.com/hooks/catch/15387298/3tmuyca', {
+    user_input: user_input
+  });
 });
 
 slackApp.command('/delete', async ({ command, ack, client }) => {
-  ack(); // Acknowledge the command immediately
+  await ack();
 
-  try {
-    const channelId = command.channel_id;
-    const result = await client.conversations.history({
-      channel: channelId,
-    });
-    const messages = result.messages;
-
-    // Delete each message asynchronously
-    messages.forEach(async (message) => {
-      try {
-        await client.chat.delete({
-          channel: channelId,
-          ts: message.ts,
-        });
-      } catch (error) {
-        console.error(`Error deleting message: ${error}`);
-      }
-    });
-
-    await client.chat.postMessage({
-      channel: channelId,
-      text: 'Chat history deleted.',
-    });
-  } catch (error) {
-    console.error('Error deleting chat history:', error);
-  }
+  const result = await client.chat.delete({
+    channel: command.channel_id,
+    ts: command.ts,
+  });
 });
 
 (async () => {
