bfb50cc720d2da271b167a52eb2a1cc3d55b0311
Chi <cheez2012@gmail.com>
2023-05-29 17:26:23 -0700

    removed commentsfor no reason - gpt4 fixed everything allegedly

--- app.js
+++ app.js
@@ -1,64 +1,58 @@
-require('dotenv').config(); // to load the environment variables
+require('dotenv').config();
 
 const { App, LogLevel } = require('@slack/bolt');
 
-// Initializes your app with your bot token and signing secret from environment variables
-const slackApp = new App({  // renamed to slackApp
+const slackApp = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  logLevel: LogLevel.DEBUG, // Set to a desired log level (optional)
+  logLevel: LogLevel.DEBUG,
 });
 
-// Event listener for app home opened event
-slackApp.event('app_home_opened', async ({ event, client, logger }) => {
-    // Push a view to the Home tab
-    await client.views.publish({
-      // the user that opened your app's app home
-      user_id: event.user,
-      // the view object that appears in the app home
-      view: {
-        type: 'home',
-        callback_id: 'home_view',
-        // body of the view
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: '*Welcome to your _App\'s Home_* :tada:',
-            },
+slackApp.event('app_home_opened', async ({ event, client }) => {
+  await client.views.publish({
+    user_id: event.user,
+    view: {
+      type: 'home',
+      callback_id: 'home_view',
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: '*Welcome to your _App\'s Home_* :tada:',
           },
-          {
-            type: 'divider',
+        },
+        {
+          type: 'divider',
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app.",
           },
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app.",
-            },
-          },
-          {
-            type: 'actions',
-            elements: [
-              {
-                type: 'button',
-                text: {
-                  type: 'plain_text',
-                  text: 'Click me!',
-                },
+        },
+        {
+          type: 'actions',
+          elements: [
+            {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'Click me!',
               },
-            ],
-          },
-        ],
-      },
-    });
+              action_id: 'click_me',
+            },
+          ],
+        },
+      ],
+    },
+  });
 });
 
-// Function to get user info from user_id
 async function getUserInfo(client, userId) {
   let retries = 0;
-  const maxRetries = 5; // maximum number of retries
+  const maxRetries = 5;
   while (retries < maxRetries) {
     try {
       const response = await client.users.info({ user: userId });
@@ -68,7 +62,6 @@ async function getUserInfo(client, userId) {
         return null;
       }
     } catch (error) {
-      logger.error('error in getuserinfo function')
       if (error.response.headers['retry-after']) {
         await new Promise((resolve) =>
           setTimeout(resolve, parseInt(error.response.headers['retry-after']) * 1000)
@@ -77,31 +70,17 @@ async function getUserInfo(client, userId) {
         throw error;
       }
     }
-
     retries++;
   }
   throw new Error('Maximum retry attempts exceeded');
 }
-  
-// Message event listener for "hi" command
+
 slackApp.message('hi', async ({ message, say, client }) => {
   const user = await getUserInfo(client, message.user);
   const fullName = user && user.profile.real_name;
     
-  // Object used to create a message that greets the user with their full name
-  const greetingMessage = {
-    text: `Hello, ${fullName}!`,
-    blocks: [
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `Hello, ${fullName}!`,
-        },
-      },
-    ],
-  };
-  
+  const greetingMessage = `Hello, ${fullName}!`;
+
   const buttonMessage = {
     blocks: [
       {
@@ -137,107 +116,69 @@ slackApp.message('hi', async ({ message, say, client }) => {
 
   say(greetingMessage);
   say(buttonMessage);
+});
 
-  const { body: greetingResponse } = greetingMessage; // Store the response of the greeting message
-  const greetingMessageId = greetingResponse.message.ts; // Get the message ID of the greeting message
-
-  const { body: buttonResponse } = buttonMessage; // Store the response of the button message
-  const buttonMessageId = buttonResponse.message.ts; // Get the message ID of the button message
-    
-  slackApp.action('Create SoW', async ({ ack, body, respond, say }) => {
-    try {
-      await ack(); // Acknowledge the action
-
-      // Prompt the user for the company name
-      await say({
-        text: "Please provide the name of the company we're doing this project for:",
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: 'Please provide the name of the company we\'re doing this project for:',
-            },
-            accessory: {
-              type: 'plain_text_input',
-              action_id: 'company_name_input',
-              placeholder: {
-                type: 'plain_text',
-                text: 'Company name',
-              },
+slackApp.action('Create SoW', async ({ ack, body, respond, logger }) => {
+  try {
+    await ack();
+    await respond({
+      text: "Please provide the name of the company we're doing this project for:",
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: 'Please provide the name of the company we\'re doing this project for:',
+          },
+          accessory: {
+            type: 'plain_text_input',
+            action_id: 'company_name_input',
+            placeholder: {
+              type: 'plain_text',
+              text: 'Company name',
             },
           },
-        ],
-      });
-    } catch (error) {
-      logger.error(`Error in 'Create SoW' action: ${error.message} Stack: ${error.stack}`);
-    }
-  });
+        },
+      ],
+    });
+  } catch (error) {
+    logger.error(`Error in 'Create SoW' action: ${error.message} Stack: ${error.stack}`);
+  }
+});
 
-  // Listen for user's response to the company name prompt
-  slackApp.action('company_name_input', async ({ ack, body, respond, say }) => {
-    try {
-      await ack(); // Acknowledge the action
+slackApp.action('company_name_input', async ({ ack, body, respond, logger }) => {
+  try {
+    await ack();
+    const companyName = body.actions[0].value;
+    await respond("Checking the database for:");
+    await respond(`- ${companyName}`);
+  } catch (error) {
+    logger.error(`Error in 'company_name_input' action: ${error.message} Stack: ${error.stack}`);
+  }
+});
 
-      const companyName = body.actions[0].value; // Get the company name entered by the user
-
-      // Send the initial message to indicate checking the database
-      await say("Checking the database for:");
-
-      // Send the second message with the company name
-      await say(`- ${companyName}`);
-    } catch (error) {
-      logger.error(`Error in 'company_name_input' action: ${error.message} Stack: ${error.stack}`);
-    }
-  });
-  
-    // Delete original messages after a certain duration
-    setTimeout(async () => {
-      await Promise.all([
-        client.chat.delete({
-          channel: message.channel,
-          ts: greetingMessageId,
-        }),
-        client.chat.delete({
-          channel: message.channel,
-          ts: buttonMessageId,
-        }),
-      ]);
-    }, 5000);
-  });
-
-// Command listener for "/delete"
 slackApp.command('/delete', async ({ command, client }) => {
   try {
     const channelId = command.channel_id;
-
-    // Retrieve chat history
     const result = await client.conversations.history({
       channel: channelId,
     });
-
     const messages = result.messages;
-
-    // Delete each message
     for (const message of messages) {
       await client.chat.delete({
         channel: channelId,
         ts: message.ts,
       });
     }
-
-    // Respond to the command with a success message
     await client.chat.postMessage({
       channel: channelId,
       text: 'Chat history deleted.',
     });
   } catch (error) {
-    // Handle errors
     console.error('Error deleting chat history:', error);
   }
 });
 
-// Start your app
 (async () => {
   await slackApp.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
