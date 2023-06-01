d046cd746da3c5275418ee147a8cf9f3ffd959ae
Chi <cheez2012@gmail.com>
2023-06-01 08:47:11 -0700

    hh

--- app.js
+++ app.js
@@ -191,13 +191,16 @@ slackApp.action('open_modal_button', async ({ ack, body, client }) => {
   }
 })
 
-slackApp.message('hi', async ({ message, client, context }) => {
+slackApp.message('hi', async ({ message, client, context, body }) => {
   const user = await getUserInfo(client, message.user);
   const fullName = user && user.profile.real_name;
   try {
     const result = await slackApp.client.chat.postMessage({
       token: context.botToken,
-      // include a button in the message
+      // ts of message to update
+      ts: body.message.ts,
+      // Channel of message
+      channel: body.channel.id,
       text: `Hello, ${fullName}!\n It's good to see you 😇. What do you want to do today?\n Pick one⬇️`,
       //respond hello with user's full name - ask what they want to do -> pick one
       blocks: [
