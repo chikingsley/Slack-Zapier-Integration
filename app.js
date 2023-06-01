0d6acfbee81bb5e8153aa9245e6572fe9c8534c1
Chi <cheez2012@gmail.com>
2023-06-01 04:36:49 -0700

    lots of new things in here - will foll back if it is broken

--- app.js
+++ app.js
@@ -1,101 +1,11 @@
 require('dotenv').config();
 const axios = require('axios');
-const { App, LogLevel } = require('@slack/bolt');
+const { App, LogLevel, contextBuiltinKeys } = require('@slack/bolt');
 
 const slackApp = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   logLevel: LogLevel.DEBUG,
-});
-
-slackApp.command('/delete', async ({ command, ack, client }) => {
-  try {
-    await ack();
-    const deleteResult = await client.chat.delete({
-      channel: command.channel_id,
-      ts: command.ts,
-    });
-    console.log('Message deleted:', deleteResult);
-  } catch (error) {
-    console.error('Error deleting message:', error);
-  }
-});
-
-slackApp.event('app_home_opened', async ({ event, client }) => {
-  await client.views.publish({
-    user_id: event.user,
-    view: {
-      type: 'home',
-      callback_id: 'home_view',
-      blocks: [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: '*Welcome to your _App\'s Home_* :tada:',
-          },
-        },
-        {
-          type: 'divider',
-        },
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: "Click the button below to open the modal.",
-          },
-        },
-        {
-          type: 'actions',
-          elements: [
-            {
-              type: 'button',
-              text: {
-                type: 'plain_text',
-                text: 'Open Modal',
-              },
-              action_id: 'open_modal_button',
-            },
-          ],
-        },
-      ],
-    },
-  });
-});
-
-slackApp.action('open_modal_button', async ({ ack, body, client, respond }) => {
-  await ack();
-  await client.views.open({
-    trigger_id: body.trigger_id,
-    view: {
-      type: 'modal',
-      title: {
-        type: 'plain_text',
-        text: 'Sample Modal',
-      },
-      close: {
-        type: 'plain_text',
-        text: 'Close',
-      },
-      submit: {
-        type: "plain_text",
-        text: "Submit"
-      },
-      blocks: [
-        {
-          type: 'input',
-          label: {
-            type: 'plain_text',
-            text: 'Label'
-          },
-          element: {
-            type: 'plain_text_input',
-            action_id: 'company_name_input',
-          },
-        }
-      ],
-    },
-  });
 });
 
 async function getUserInfo(client, userId) {
@@ -123,79 +33,252 @@ async function getUserInfo(client, userId) {
   throw new Error('Maximum retry attempts exceeded');
 }
 
+// Call views.open with the built-in client
+// MAKE MODAL OBJECT CALLED CHICKEN - view1 callback_id
+const chicken = client.views.open({
+  // Pass a valid trigger_id within 3 seconds of receiving it
+  trigger_id: body.trigger_id,
+  // View payload
+  view: {
+    type: 'modal',
+    // View identifier - USE IN MODAL CALLBACKS
+    callback_id: 'view_1',
+    private_metadata: 'my neck, my back...',
+    title: {
+      type: 'plain_text',
+      text: 'Modal title',
+    },
+    // Submit button
+    submit: {
+      type: 'plain_text',
+      text: 'Submit',
+    },
+    // Close Button
+    close: {
+      type: 'plain_text',
+      text: 'Close',
+    },
+    blocks: [
+      // User Input Block
+      {
+        type: 'input',
+        block_id: 'company_name_block', 
+        // Label/Title above input field 
+        label: {
+          type: 'plain_text',
+          text: 'Company name',
+        },
+        // Actual Input Field
+        element: {
+          type: 'plain_text_input',
+          action_id: 'company_name_input',
+        },
+        // Input Field Placeholder text
+        element: {
+          type: 'plain_text_input',
+          action_id: 'input1',
+          placeholder: {
+            type: 'plain_text',
+            text: 'Type in here',
+          }
+        }
+      },
+    ],
+  },
+});
+
+//basic listener that publishes a view to the home tab where app lives
+slackApp.event('app_home_opened', async ({ event, client, context }) => {
+  try {
+  // view.publish is the method that the app uses to push a view to the home tab
+    await client.views.publish({
+      // the user that opened your app's app home
+      user_id: event.user,
+      // the view object taht appears in teh app home
+      view: {
+        type: 'home',
+        callback_id: 'home_view',
+        //the body of the view
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: '*Welcome to your _App\'s Home_* :tada:',
+            },
+          },
+          {
+            type: 'divider',
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: "Click the button below to open the modal.",
+            },
+          },
+          {
+            type: 'actions',
+            elements: [
+              {
+                type: 'button',
+                text: {
+                  type: 'plain_text',
+                  text: 'Open Modal',
+                },
+                action_id: 'open_modal_button',
+              },
+            ],
+          },
+        ],
+      },
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+slackApp.action('open_modal_button', async ({ ack, body, client, respond }) => {
+  await ack();
+
+  //open modal to get user feedback
+  try {
+    await client.views.open({
+      trigger_id: body.trigger_id,
+      view: {
+        type: 'modal',
+        title: {
+          type: 'plain_text',
+          text: 'Sample Modal',
+        },
+        close: {
+          type: 'plain_text',
+          text: 'Close',
+        },
+        submit: {
+          type: "plain_text",
+          text: "Submit"
+        },
+        blocks: [
+          {
+            type: 'input',
+            label: {
+              type: 'plain_text',
+              text: 'Label'
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'company_name_input',
+            },
+          }
+        ],
+      },
+    });
+  }
+  catch(error) {
+    console.error(error);
+  }
+})
+
 slackApp.message('hi', async ({ message, say, client }) => {
   const user = await getUserInfo(client, message.user);
   const fullName = user && user.profile.real_name;
-  const greetingMessage = `Hello, ${fullName}!`;
-  await say(greetingMessage);
-  const blocks = [
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: "It's good to see you 😇. What do you want to do today?",
-      },
-    },
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: 'Pick one⬇️',
-      },
-    },
-    {
-      type: 'actions',
-      elements: [
+  
+  try {
+    // respond hello with user's full name - ask what they want to do -> pick one
+    await respond({
+      text: `Hello, ${fullName}!\n It's good to see you 😇. What do you want to do today?`,
+      blocks: [
         {
-          type: 'button',
+          type: 'section',
           text: {
-            type: 'plain_text',
-            text: 'Create SoW',
+            type: 'mrkdwn',
+            text: `Hello, ${fullName}!\n It's good to see you 😇. What do you want to do today?`,
           },
-          action_id: 'Create_SoW',
         },
       ],
-    },
-  ];
-  await say({ blocks });
+    });
+    // tell user to pick an option
+    await respond({
+      text: `Pick one⬇️`,
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `Pick one⬇️`,
+          },
+        },
+      ],
+    });
+    // button option encoded in blocks
+    const options = [
+      {
+        type: 'actions',
+        elements: [
+          {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'Create SoW',
+            },
+            action_id: 'Create_SoW',
+          },
+        ],
+      },
+    ];
+    await say({ options });
+  }
+  catch (error) {
+    console.error(error);
+  }
 });
 
-slackApp.action('Create_SoW', async ({ ack, body, client, respond, say }) => {
+slackApp.action('Create_SoW', async ({ ack, body, client, respond, say, context }) => {
+  // acknowledge the button request
   await ack();
-  // Disable the button and replace it with a message
-  await respond({
-    replace_elements: [
-      {
-        type: 'button',
-        action_id: 'Create_SoW',
-      },
-    ],
-    text: "Processing...",
-    blocks: [
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: 'Processing...',
-        },
-      },
-    ],
-  });
 
-  // Ask the user to make a Statement of Work (SoW)
+  try {
+    // update the button message
+    const result = await slackApp.client.chat.update({
+      token: context.botToken,
+      // ts of message to update
+      ts: body.message_ts,
+      // Channel to send message to
+      channel: body.channel.id,
+      // Message
+      //replace_original: true,
+      text: "Processing...",
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `Processing... ${Create_SoW} button click`,
+          },
+        },
+      ],
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+
+  // Ask the user to make a Statement of Work (SoW) - let them know to enter company name in dialogue box
   await respond({
-    text: "Okay - let's make a Statement of Work (SoW)!!",
+    text: `Okay - let's make a Statement of Work (SoW)!!\n Who are we doing this project for? Respond with a company name or the name of the point of contact (POC).`,
     blocks: [
       {
         type: 'section',
         text: {
           type: 'mrkdwn',
-          text: "Okay - let's make a Statement of Work (SoW)!!",
+          text: `Okay - let's make a Statement of Work (SoW)!!\n Who are we doing this project for? Respond with a company name or the name of the point of contact (POC).`,
         },
       },
     ],
   });
-  
 
   // Ask the user for the company name or POC using blocks
   await respond({
@@ -210,50 +293,24 @@ slackApp.action('Create_SoW', async ({ ack, body, client, respond, say }) => {
       },
     ],
   });
-  
-  // Open the modal
-  await client.views.open({
-    trigger_id: body.trigger_id,
-    view: {
-      type: 'modal',
-      callback_id: 'sow_modal',
-      title: {
-        type: 'plain_text',
-        text: 'Create a SoW',
-      },
-      submit: {
-        type: 'plain_text',
-        text: 'Submit',
-      },
-      close: {
-        type: 'plain_text',
-        text: 'Close',
-      },
-      blocks: [
-        {
-          type: 'input',
-          block_id: 'company_name_block',  // Add this line
-          label: {
-            type: 'plain_text',
-            text: 'Company name',
-          },
-          element: {
-            type: 'plain_text_input',
-            action_id: 'company_name_input',
-          },
-        },
-      ],
-    },
-  });
-await say({ text: 'The modal has been opened.' });
+
+  // Open Modal to get user input
+  try {
+    await client.views.open({
+      trigger_id: body.trigger_id,
+      //view payload and call modal
+      view: chicken
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
 });
 
-slackApp.view('sow_modal', async ({ ack, body, view, client }) => {
+slackApp.view('view_1', async ({ ack, view, client }) => {
   await ack();
   const user_input = view.state.values.company_name_block.company_name_input.value;
-  const channelId = body.channel.id; // Get the channel ID from the body object
-  await client.chat.postMessage({
-    channel: channelId,
+  await client.chat.postMessage({ //sending metadata out
     text: `The submitted value is: ${user_input}`,
   });
   axios.post('https://eowdv9m1ufg1knl.m.pipedream.net', {
